206816ecc1558ca4c8840e7dfd5f431a30fb41ea
Jackelin <jackelinvasquezguzman@gmail.com>
2025-07-24 16:05:11 -0600

    Menús

---
+++ Actividad 7.py
@@ -0,0 +1,19 @@
+
+def menu():
+    print("1.Ingresar una lista de n números reales y mostrar.")
+    print("2.Calcular el area y perimetro de un rectangulo.")
+    print("3.Vereficar si un número es primo")
+    print("4.Calcular el promedio de calificaciones")
+    print("5.Ingresar una lista.")
+    print("6.Simular una calculadora de operaiones")
+    print("7.Salir del programa")
+
+def sub_menu1():
+    print("1.Suma total.\n2.Promedio.\n3.Cantidad de positivos, negativos y ceros.\n4.Cuantos son multiplos de 3.")
+
+def menu_opcion4():
+    print("1.Cuantas son mayores  o iguales a 85.\n2.Cuantas estan en zona de riesgo(menor a 60.")
+
+def menu_opcion5():
+    print("1.El númeor mayor.\n2.El número menor.\n3.Cuantos se repiten")
+
